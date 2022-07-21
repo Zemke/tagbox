58f2c4c531bb98add1c004c58ecc1f893dbaa26c
Zemke <florian@zemke.io>
2022-07-21 22:57:19 +0200

    Cosmetics

--- js.js
+++ js.js
@@ -37,126 +37,106 @@ class TagBox extends HTMLElement {
     super();
     const shadow = this.attachShadow({ mode: 'open' });
     shadow.innerHTML = `
-<style>
-  .dummy {
-    position: absolute;
-    visibility: hidden;
-    pointer-events: none;
-    white-space: pre;
-  }
-  .chat-container {
-    position: relative;
-  }
-  .suggestions {
-    min-width: auto;
-    background-clip: border-box;
-    box-shadow: .4rem .9rem 1.5rem #000;
-  }
-  .suggestions img {
-    height: 1rem;
-  }
-  .offsets {
-    position: absolute;
-    pointer-events: none;
-    overflow: hidden;
-    top: 0;
-  }
-  .offset {
-    background-color: rgba(99, 1, 45, 0.35);
-    pointer-events: none;
-    position: absolute;
-    top: .2rem;
-    bottom: .2rem;
-    z-index: 99;
-    border-top-left-radius: 1rem;
-    border-bottom-left-radius: 1rem;
-    border-top-right-radius: .5rem;
-    border-bottom-right-radius: .5rem;
-  }
-  #dropdown {
-    display: none;
-    position: absolute;
-    top: 100%;
-    left: 0;
-    z-index: 1000;
-    display: none;
-    float: left;
-    min-width: 10rem;
-    padding: .5rem 0;
-    margin: .125rem 0 0;
-    font-size: 1rem;
-    color: #212529;
-    text-align: left;
-    list-style: none;
-    background-color: #fff;
-    background-clip: padding-box;
-    border: 1px solid rgba(0,0,0,.15);
-    border-radius: .25rem;
-    min-width: auto;
-    background-clip: border-box;
-    box-shadow: .4rem .9rem 1.5rem #000;
-  }
-  #dropdown.show {
-    display: block;
-  }
-  .dropdown-item {
-    display: block;
-    width: 100%;
-    padding: .25rem 1.5rem;
-    padding-right: 1.5rem;
-    padding-left: 1.5rem;
-    clear: both;
-    font-weight: 400;
-    color: #212529;
-    text-align: inherit;
-    white-space: nowrap;
-    background-color: transparent;
-    border: 0;
-  }
-  .dropdown-item.active {
-    color: #fff;
-    text-decoration: none;
-    background-color: #322a21;
-  }
-</style>
+      <style>
+        .dummy {
+          display: inline;
+          position: absolute;
+          visibility: hidden;
+          pointer-events: none;
+          white-space: pre;
+        }
+        .chat-container {
+          position: relative;
+        }
+        .suggestions {
+          min-width: auto;
+          background-clip: border-box;
+          box-shadow: .4rem .9rem 1.5rem #000;
+        }
+        .suggestions img {
+          height: 1rem;
+        }
+        .offsets {
+          position: absolute;
+          pointer-events: none;
+          overflow: hidden;
+          top: 0;
+        }
+        .offset {
+          background-color: rgba(99, 1, 45, 0.35);
+          pointer-events: none;
+          position: absolute;
+          top: .2rem;
+          bottom: .2rem;
+          z-index: 99;
+          border-top-left-radius: 1rem;
+          border-bottom-left-radius: 1rem;
+          border-top-right-radius: .5rem;
+          border-bottom-right-radius: .5rem;
+        }
+        #dropdown {
+          display: none;
+          position: absolute;
+          top: 100%;
+          left: 0;
+          z-index: 1000;
+          display: none;
+          float: left;
+          min-width: 10rem;
+          padding: .5rem 0;
+          margin: .125rem 0 0;
+          font-size: 1rem;
+          color: #212529;
+          text-align: left;
+          list-style: none;
+          background-color: #fff;
+          background-clip: padding-box;
+          border: 1px solid rgba(0,0,0,.15);
+          border-radius: .25rem;
+          min-width: auto;
+          background-clip: border-box;
+          box-shadow: .4rem .9rem 1.5rem #000;
+        }
+        #dropdown.show {
+          display: block;
+        }
+        .dropdown-item {
+          display: block;
+          width: 100%;
+          padding: .25rem 1.5rem;
+          padding-right: 1.5rem;
+          padding-left: 1.5rem;
+          clear: both;
+          font-weight: 400;
+          color: #212529;
+          text-align: inherit;
+          white-space: nowrap;
+          background-color: transparent;
+          border: 0;
+        }
+        .dropdown-item.active {
+          color: #fff;
+          text-decoration: none;
+          background-color: #322a21;
+        }
+      </style>
 
-<div class="chat-container">
-  <input type="text"
-         name="chat-input"
-         id="chatInput">
-  <div id="offsets" class="offsets">
-    <div #recipients
-         class="offset"
-         [ngStyle]="tag.style"
-         [attr.data-id]="tag.user.id"
-         [attr.data-username]="tag.user.username">
-    </div>
-  </div>
-  <div id="dummy" class="dummy d-inline"></div>
-  <div id="dropdown" [class.show]="suggestions">
-    <small class="form-text text-muted mx-2"
-           *ngIf="!suggestions?.length">No such user.</small>
-    <button *ngFor="let sugg of suggestions; let first = first;"
-            #suggestions
-            class="dropdown-item px-2" [class.active]="first"
-            type="button" [value]="sugg.id" (click)="complete(sugg, true)">
-      {{ sugg.username }}
-    </button>
-  </div>
-</div>
-`
+      <div class="chat-container">
+        <input type="text"
+               name="chat-input"
+               id="chatInput">
+        <div id="offsets" class="offsets">
+        </div>
+        <div id="dummy" class="dummy">
+        </div>
+        <div id="dropdown">
+        </div>
+      </div>
+      `
 
-    shadow.getElementById('chatInput').addEventListener('keydown', e => {
-      this.onKeydown(e);
-    });
-
-    shadow.getElementById('chatInput').addEventListener('keyup', e => {
-      this.onKeyup(e);
-    });
-
-    shadow.getElementById('chatInput').addEventListener('input', e => {
-      this.onInput(e);
-    });
+    shadow.getElementById('chatInput').addEventListener('keydown', e => { this.onKeydown(e); });
+    shadow.getElementById('chatInput').addEventListener('keyup', e => { this.onKeyup(e); });
+    shadow.getElementById('chatInput').addEventListener('input', e => { this.onInput(e); });
   }
 
   get chatInputEl() {
@@ -224,8 +204,10 @@ class TagBox extends HTMLElement {
       html += `<div class="offset" style="${style}" data-id="${tag.user.id}"></div>`;
     }
     this.offsetsEl.nativeElement.innerHTML = html;
+    // TODO "no such user found"
   }
 
+  // TODO get rid of this (Angular relict)
   wrap(id) {
     return { 'nativeElement': this.shadowRoot.getElementById(id) };
   }
@@ -281,45 +263,45 @@ class TagBox extends HTMLElement {
   }
 
   styleOffsetsEl() {
-      const {width, height} = this.chatInputEl.nativeElement.getBoundingClientRect();
-      const {paddingLeft, paddingRight} = window.getComputedStyle(this.chatInputEl.nativeElement);
-      this.paddingLeft = parseFloat(paddingLeft);
-      this.offsetsEl.nativeElement.style.width =
-        width - this.paddingLeft - parseFloat(paddingRight) + 'px';
-      this.offsetsEl.nativeElement.style.marginLeft = paddingLeft;
-      this.offsetsEl.nativeElement.style.marginRight = paddingRight;
-      this.offsetsEl.nativeElement.style.height = height + 'px';
+    const {width, height} = this.chatInputEl.nativeElement.getBoundingClientRect();
+    const {paddingLeft, paddingRight} = window.getComputedStyle(this.chatInputEl.nativeElement);
+    this.paddingLeft = parseFloat(paddingLeft);
+    this.offsetsEl.nativeElement.style.width =
+      width - this.paddingLeft - parseFloat(paddingRight) + 'px';
+    this.offsetsEl.nativeElement.style.marginLeft = paddingLeft;
+    this.offsetsEl.nativeElement.style.marginRight = paddingRight;
+    this.offsetsEl.nativeElement.style.height = height + 'px';
   }
 
   styleDummyEl() {
-      const {fontSize, fontFamily} = window.getComputedStyle(this.chatInputEl.nativeElement);
-      this.dummyEl.nativeElement.style.fontSize = fontSize;
-      this.dummyEl.nativeElement.style.fontFamily = fontFamily;
+    const {fontSize, fontFamily} = window.getComputedStyle(this.chatInputEl.nativeElement);
+    this.dummyEl.nativeElement.style.fontSize = fontSize;
+    this.dummyEl.nativeElement.style.fontFamily = fontFamily;
   }
 
   styleDropdownEl(q = null, v = null) {
-      if (this.dropdownEl?.nativeElement == null) return;
-      if (q == null || v == null) {
-          [q, v] = this.getProc();
-      }
-      if (q == null || v == null) return;
-      this.dropdownEl.nativeElement.style.left = Math.min(
-          this.getOffset(v.substring(0, v.length-q.length)) - this.chatInputEl.nativeElement.scrollLeft,
-          window.innerWidth - 200) + 'px';
+    if (this.dropdownEl?.nativeElement == null) return;
+    if (q == null || v == null) {
+      [q, v] = this.getProc();
+    }
+    if (q == null || v == null) return;
+    this.dropdownEl.nativeElement.style.left = Math.min(
+      this.getOffset(v.substring(0, v.length-q.length)) - this.chatInputEl.nativeElement.scrollLeft,
+      window.innerWidth - 200) + 'px';
   }
 
   complete(user, fromClick = false) {
-      const inpElem = this.chatInputEl.nativeElement;
-      const [q, v, caret] = this.getProc();
-      inpElem.value =
-          v.substring(0, caret - q.length)
-          + user.username
-          + inpElem.value.substring(caret);
-      this.suggestions = null;
-      fromClick && inpElem.focus();
-      inpElem.selectionStart = caret - q.length + user.username.length;
-      inpElem.selectionEnd = inpElem.selectionStart;
-      this.updateRecipients();
+    const inpElem = this.chatInputEl.nativeElement;
+    const [q, v, caret] = this.getProc();
+    inpElem.value =
+      v.substring(0, caret - q.length)
+      + user.username
+      + inpElem.value.substring(caret);
+    this.suggestions = null;
+    fromClick && inpElem.focus();
+    inpElem.selectionStart = caret - q.length + user.username.length;
+    inpElem.selectionEnd = inpElem.selectionStart;
+    this.updateRecipients();
   }
 
   onKeydown(e) {
@@ -355,15 +337,15 @@ class TagBox extends HTMLElement {
   }
 
   onKeyup(e) {
-      const key = e.key === 'Unidentified' ? String.fromCharCode(e.which) : e.key;
-      if (key.length > 1 && !['ArrowDown', 'ArrowUp', 'Tab', 'Enter', 'Backspace', 'Delete'].includes(key)) {
-          this.suggest();
-      }
+    const key = e.key === 'Unidentified' ? String.fromCharCode(e.which) : e.key;
+    if (key.length > 1 && !['ArrowDown', 'ArrowUp', 'Tab', 'Enter', 'Backspace', 'Delete'].includes(key)) {
+      this.suggest();
+    }
   }
 
   onInput() {
-      this.suggest();
-      setTimeout(() => this.updateRecipients());
+    this.suggest();
+    setTimeout(() => this.updateRecipients());
   }
 
   updateRecipients() {
@@ -387,14 +369,13 @@ class TagBox extends HTMLElement {
   }
 
   getOffset(v) {
-      this.dummyEl.nativeElement.textContent = v;
-      this.dummyEl.nativeElement.style.paddingLeft = `${this.paddingLeft}px`;
-      // scrollLeft is subtracted in the result, it's done here just for visual reasons when debugging
-      this.dummyEl.nativeElement.style.marginLeft = `-${this.chatInputEl.nativeElement.scrollLeft}px`;
-
-      const res = this.dummyEl.nativeElement.getBoundingClientRect().width;
-      this.dummyEl.nativeElement.innerHTML = '';
-      return res;
+    this.dummyEl.nativeElement.textContent = v;
+    this.dummyEl.nativeElement.style.paddingLeft = `${this.paddingLeft}px`;
+    // scrollLeft is subtracted in the result, it's done here just for visual reasons when debugging
+    this.dummyEl.nativeElement.style.marginLeft = `-${this.chatInputEl.nativeElement.scrollLeft}px`;
+    const res = this.dummyEl.nativeElement.getBoundingClientRect().width;
+    this.dummyEl.nativeElement.innerHTML = '';
+    return res;
   }
 
   suggest() {
@@ -418,18 +399,18 @@ class TagBox extends HTMLElement {
    *   until associated @ sign and the caret index.
    */
   getProc() {
-      const {selectionStart, value} = this.chatInputEl.nativeElement;
-      const v = value.substring(0, selectionStart);
-      if (v.indexOf('@') === -1) return [null, v, selectionStart];
-      const rev = v.split("").reverse()
-      const qRev = rev.slice(0, rev.indexOf("@"))
-      const charBefAt = rev.slice(0, rev.indexOf("@")+2).pop();
-      if (charBefAt != null && TagBox.DELIMITER.test(charBefAt)) {
-          return [null, v, selectionStart];
-      }
-      const q = qRev.reverse().join('')
-      if (!TagBox.DELIMITER.test(q)) return [null, v, selectionStart]
-      return [q, v, selectionStart];
+    const {selectionStart, value} = this.chatInputEl.nativeElement;
+    const v = value.substring(0, selectionStart);
+    if (v.indexOf('@') === -1) return [null, v, selectionStart];
+    const rev = v.split("").reverse()
+    const qRev = rev.slice(0, rev.indexOf("@"))
+    const charBefAt = rev.slice(0, rev.indexOf("@")+2).pop();
+    if (charBefAt != null && TagBox.DELIMITER.test(charBefAt)) {
+      return [null, v, selectionStart];
+    }
+    const q = qRev.reverse().join('')
+    if (!TagBox.DELIMITER.test(q)) return [null, v, selectionStart]
+    return [q, v, selectionStart];
   }
 }
 
