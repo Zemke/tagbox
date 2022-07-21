f21a7e50de5b560e1351652b643972729f767f0e
Zemke <florian@zemke.io>
2022-07-21 16:32:24 +0200

    Copy stuff over

--- js.js
+++ js.js
@@ -124,20 +124,29 @@ class TagBox extends HTMLElement {
     });
   }
 
-  get chatInputElem() {
+  get chatInputEl() {
     return this.wrap('chatInput');
   }
 
-  get offsetsElem() {
+  get offsetsEl() {
     return this.wrap('offsets');
+  }
+
+  set suggestions(vv) {
+    if (vv == null || vv.length < 1) {
+      this.getElementsByTagName('zemke-tag-box-suggs').innerHTML = '';
+    } else {
+      console.log('TODO set suggestions to', vv);
+    }
   }
 
   get suggestions() {
     const elems = this.getElementsByTagName('zemke-tag-box-suggs');
     if (elems > 0) throw new Error('more than one zemke-tag-box-suggs');
     return Array.from(elems[0].children).map(child => ({
-      label: child.textContent,
-      value: child.getAttribute('value'),
+      // TODO rename to label and value
+      username: child.textContent,
+      id: child.getAttribute('value'),
     }));
   }
 
@@ -148,7 +157,212 @@ class TagBox extends HTMLElement {
   connectedCallback() {
     setTimeout(() => {
       console.log(this.suggestions);
+      this.allSuggestions = [...this.suggestions];
     });
+  }
+
+  submit() {
+      this.disabled = true;
+      const message = {
+          body: this.chatInputEl.nativeElement.value,
+          recipients: this.recipients,
+          category: this.recipients?.length ? 'PRIVATE' : 'SHOUTBOX',
+      };
+      this.message.emit([message, (success) => {
+          this.disabled = false;
+          if (success) {
+              this.recipients = [];
+              this.tags = [];
+              this.suggestions = null;
+              this.chatInputEl.nativeElement.value = '';
+          }
+          setTimeout(() => this.chatInputEl.nativeElement.focus());
+      }]);
+  }
+
+  styleOffsetsEl() {
+      const {width, height} = this.chatInputEl.nativeElement.getBoundingClientRect();
+      const {paddingLeft, paddingRight} = window.getComputedStyle(this.chatInputEl.nativeElement);
+      this.paddingLeft = parseFloat(paddingLeft);
+      this.offsetsEl.nativeElement.style.width =
+          width - this.paddingLeft - parseFloat(paddingRight) + 'px';
+      this.offsetsEl.nativeElement.style.marginLeft = paddingLeft;
+      this.offsetsEl.nativeElement.style.marginRight = paddingRight;
+      this.offsetsEl.nativeElement.style.height = height + 'px';
+  }
+
+  styleDummyEl() {
+      const {fontSize, fontFamily} = window.getComputedStyle(this.chatInputEl.nativeElement);
+      this.dummyEl.nativeElement.style.fontSize = fontSize;
+      this.dummyEl.nativeElement.style.fontFamily = fontFamily;
+  }
+
+  styleDropdownEl(q = null, v = null) {
+      if (this.dropdownEl?.nativeElement == null) return;
+      if (q == null || v == null) {
+          [q, v] = this.getProc();
+      }
+      if (q == null || v == null) return;
+      this.dropdownEl.nativeElement.style.left = Math.min(
+          this.getOffset(v.substring(0, v.length-q.length)) - this.chatInputEl.nativeElement.scrollLeft,
+          window.innerWidth - 200) + 'px';
+  }
+
+  complete(user, fromClick = false) {
+      const inpElem = this.chatInputEl.nativeElement;
+      const [q, v, caret] = this.getProc();
+      inpElem.value =
+          v.substring(0, caret - q.length)
+          + user.username
+          + inpElem.value.substring(caret);
+      this.suggestions = null;
+      fromClick && inpElem.focus();
+      inpElem.selectionStart = caret - q.length + user.username.length;
+      inpElem.selectionEnd = inpElem.selectionStart;
+      this.updateRecipients();
+  }
+
+  onKeydown(e) {
+      const key = e.key === 'Unidentified' ? String.fromCharCode(e.which) : e.key;
+      if (this.suggestions?.length && ['ArrowDown', 'ArrowUp', 'Tab', 'Enter'].includes(key)) {
+          e.preventDefault();
+          const buttons = Array.from(this.suggestionsEl).map(el => el.nativeElement);
+          let active;
+          for (let i = 0; i < buttons.length; i++) {
+              if (buttons[i].classList.contains('active')) {
+                  active = i;
+                  break;
+              }
+          }
+          if (key === 'Enter') {
+              const user = this.suggestions.find(x => x.id == buttons[active].value);
+              if (user == null) return;
+              this.complete(user);
+          } else {
+              if (active == null) {
+                  buttons[0].classList.add('active');
+              } else {
+                  const up = key === 'ArrowUp' || (e.shiftKey && key === 'Tab')
+                  buttons[active].classList.remove('active');
+                  if (up && active == 0) {
+                      buttons[buttons.length-1].classList.add('active');
+                  } else {
+                      buttons[(active + (up ? -1 : +1)) % buttons.length].classList.add('active');
+                  }
+              }
+          }
+      }
+  }
+
+  onKeyup(e) {
+      const key = e.key === 'Unidentified' ? String.fromCharCode(e.which) : e.key;
+      if (key.length > 1 && !['ArrowDown', 'ArrowUp', 'Tab', 'Enter', 'Backspace', 'Delete'].includes(key)) {
+          this.suggest();
+      }
+  }
+
+  onInput() {
+      this.suggest();
+      setTimeout(() => this.updateRecipients());
+  }
+
+  async updateRecipients() {
+      const {value, scrollLeft} = this.chatInputEl.nativeElement;
+      const matchAll = Array.from(value.matchAll(/(?:^|[^a-z0-9-_])@([a-z0-9-_]+)/ig));
+      const matches = [];
+      for (const m of matchAll) {
+           let user = this.allSuggestions.find(u => u.username.toLowerCase() === m[1].toLowerCase());
+           if (user == null) {
+               await this.lazyLoadAllSuggestions();
+               // TODO double houble
+               user = this.allSuggestions.find(u => u.username.toLowerCase() === m[1].toLowerCase());
+           }
+           if (user != null) {
+               matches.push({ index: m.index + m[0].indexOf('@'), user });
+           }
+      }
+
+      this.recipients = matches.map(({user}) => user).filter((v,i,a) => a.indexOf(v) === i);
+      this.tags = matches.map(m => ({
+          user: m.user,
+          style: {
+              width: `${this.getOffset(m.user.username)+4}px`, // 4 is arbitrary width increase
+              left: `${this.getOffset(value.substring(0, m.index))-this.paddingLeft-scrollLeft}px`,
+          }
+      }));
+  }
+
+  getOffset(v) {
+      this.dummyEl.nativeElement.textContent = v;
+      this.dummyEl.nativeElement.style.paddingLeft = `${this.paddingLeft}px`;
+      // scrollLeft is subtracted in the result, it's done here just for visual reasons when debugging
+      this.dummyEl.nativeElement.style.marginLeft = `-${this.chatInputEl.nativeElement.scrollLeft}px`;
+
+      const res = this.dummyEl.nativeElement.getBoundingClientRect().width;
+      this.dummyEl.nativeElement.innerHTML = '';
+      return res;
+  }
+
+  suggest() {
+      const [q, v,] = this.getProc();
+      if (q == null) {
+          this.suggestions = null;
+          return;
+      }
+
+      this.styleDropdownEl(q,v);
+
+      this.suggestions = this.allSuggestions
+          .filter(({username}) => username.toLowerCase().startsWith(q.toLowerCase()))
+          .slice(0, this.suggestionsSlice);
+
+      if (!this.lazyLoadedSuggestions && !this.lazyLoadingSuggestions) {
+          this.lazyLoadAllSuggestions().then(this.suggest.bind(this));
+      }
+  }
+
+  lazyLoadAllSuggestions() {
+      // TODO makes sense?
+      return Promise.resolve(this.allSuggestions);
+      /*
+      if (this.lazyLoadedSuggestions) return Promise.resolve();
+      if (this.lazyLoadingSuggestions) {
+          return this.lazySuggestionsPromise;
+      }
+      this.lazyLoadingSuggestions = true;
+      return this.requestService.get<UserMinimalDto[]>("user", {minimal: "true"}).toPromise()
+          .then(users => this.allSuggestions.push(
+              ...users.filter(user =>
+                  !this.allSuggestions.map(u => u.id).includes(user.id) && user.id !== this.authUser.id)))
+          .then(() => this.lazySuggestionsResolver())
+          .finally(() => {
+              this.lazyLoadedSuggestions = true
+              this.lazyLoadingSuggestions = false;
+          });
+      */
+  }
+
+  /**
+   * Get current input typeahead process information.
+   *
+   * @param {string} Char just typed that's not yet part of the inputs
+   *   value at the moment of calling this method.
+   * @returns {Array} The current typeahead string and the string
+   *   until associated @ sign and the caret index.
+   */
+  getProc() {
+      const {selectionStart, value} = this.chatInputEl.nativeElement;
+      const v = value.substring(0, selectionStart);
+      if (v.indexOf('@') === -1) return [null, v, selectionStart];
+      const rev = v.split("").reverse()
+      const qRev = rev.slice(0, rev.indexOf("@"))
+      const charBefAt = rev.slice(0, rev.indexOf("@")+2).pop();
+      if (charBefAt != null && TagBox.DELIMITER.test(charBefAt)) {
+          return [null, v, selectionStart];
+      }
+      const q = qRev.reverse().join('')
+      if (!TagBox.DELIMITER.test(q)) return [null, v, selectionStart]
+      return [q, v, selectionStart];
   }
 }
 
